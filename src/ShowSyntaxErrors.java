28b16f8b3c9f1fb273e0fa4874f70c835940b81c
AbdiJavad <Mirjavadabdi@gmail.com>
2023-10-22 00:34:54 +0330

    first commit

---
+++ src/ShowSyntaxErrors.java
@@ -0,0 +1,8 @@
+public class ShowSyntaxErrors {
+    public static void main(String[] args) {
+        //  System.out.println("Welcome to Java);
+        // ■ The string Welcome to Java should be closed with a closing quotation mark in
+        // line 3.
+        System.out.println("Welcome to Java");
+    }
+}
