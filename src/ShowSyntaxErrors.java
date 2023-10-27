8cf79193a7f2b30715d768f1e79d1e431e9ecacd
AbdiJavad <Mirjavadabdi@gmail.com>
2023-10-27 13:43:35 +0330

    Updated ShowSyntaxErro

--- src/ShowSyntaxErrors.java
+++ src/ShowSyntaxErrors.java
@@ -1,8 +1,6 @@
 public class ShowSyntaxErrors {
     public static void main(String[] args) {
-        //  System.out.println("Welcome to Java);
-        // ■ The string Welcome to Java should be closed with a closing quotation mark in
-        // line 3.
-        System.out.println("Welcome to Java");
+       //System.out.println("Welcome to Java);
+         System.out.println("Welcome to Java");
     }
 }
