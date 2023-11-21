e14d23847881fb746b562844bf61fef34504b432
AbdiJavad <Mirjavadabdi@gmail.com>
2023-11-21 21:32:53 +0330

    Updated

--- src/ComputeExpression.java
+++ src/ComputeExpression.java
@@ -1,5 +1,5 @@
 public class ComputeExpression {
-    
+
     public static void main(String[] args) {
         System.out.println("(10.5 + 2 * 3) / (45 – 3.5) = ");
         System.out.println((10.5 + 2 * 3) / (45-3.5));
--- src/ShowRuntimeErrors.java
+++ src/ShowRuntimeErrors.java
@@ -1,4 +1,5 @@
 public class ShowRuntimeErrors{
+    
     public static void main(String[] args) {
         System.out.println(1 / 0);
     }
